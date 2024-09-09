bdc6643a992b5c289b5f3952e4cc3ad8c8c83192
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-09-09 13:54:10 -0400

    I updated user-routes file

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
 const {
-  getUsers,
-  getSingleUser,
   createUser,
-} = require('../../controllers/userController');
+  getSingleUser,
+  login,
+} = require('../../controllers/user-controller');
 
+// import middleware
+const { authMiddleware } = require('../../utils/auth');
 
-router.route('/').get(getUsers).post(createUser);
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(authMiddleware);
 
+router.route('/login').post(login);
 
-router.route('/:userId').get(getSingleUser);
+router.route('/me').get(authMiddleware, getSingleUser);
 
 module.exports = router;
