08f590b584c7cc8d3b2b79fdb642374485eb63a4
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-09-10 11:58:39 -0400

    I updated the index files in api folder and models folder

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const itemRoutes = require('./itemRoute');
+const eraRoutes = require('./eraRoutes');
+const cartRoutes = require('./cartRoutes');
 
 
 router.use('/users', userRoutes);
 router.use('/items', itemRoutes);
-console.log("api routes inside");
+router.use('/eras', eraRoutes);
+router.use('/cart', cartRoutes);
 
 
 module.exports = router;
