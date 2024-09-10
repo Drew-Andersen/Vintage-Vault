a41e5d9028befa6e349875e2c3c30c849b4714e6
Drew-Andersen <andrewtandersen@gmail.com>
2024-09-10 12:04:01 -0400

    Creating a Carousel for the homepage

--- client/src/pages/Era70s.jsx
+++ client/src/pages/Era70s.jsx
@@ -1,5 +1,18 @@
-export default function Era70s (){
-    return(
-        <h1 className="text-center">70's Era</h1>
+export default function Era70s() {
+    return (
+        <>
+            <section className="item-section">
+                <h3 className="text-center my-4">70's Era</h3>
+                <div className="items-grid">
+                    {[...Array(6)].map((_, i) => (
+                        <div className="item-card" key={i}>
+                            <div className="item-image"></div>
+                            <p>Description</p>
+                            <p>$$$$$</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
     )
 }
