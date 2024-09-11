0430730048e4b2268f22819e49ce2ad3dcd513d8
Xavier <xavier3435@icloud.com>
2024-09-11 13:33:51 -0400

    added classes to era jsx & added styling

--- client/src/pages/Era00s.jsx
+++ client/src/pages/Era00s.jsx
@@ -1,18 +1,24 @@
-export default function Era00s (){
-    return(
+import React from 'react';
+import './Era00s.css'; 
+
+export default function Era00s({ items }) {
+    return (
         <>
             <section className="item-section">
                 <h3 className="text-center my-4">00's Era</h3>
                 <div className="items-grid">
-                    {[...Array(6)].map((_, i) => (
+                    {items.map((item, i) => (
                         <div className="item-card" key={i}>
-                            <div className="item-image"></div>
-                            <p>Description</p>
-                            <p>$$$$$</p>
+                            <div className="item-image">
+                                <img src={item.imageUrl} alt={item.name} />
+                            </div>
+                            <h4>{item.name}</h4>
+                            <p>{item.description}</p>
+                            <p className="price">{item.price}</p>
                         </div>
                     ))}
                 </div>
             </section>
         </>
-    )
+    );
 }
