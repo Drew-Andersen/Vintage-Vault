ef38ebd8adf9fadc321f24b077ea702efe6ed66f
Drew-Andersen <andrewtandersen@gmail.com>
2024-11-05 12:06:59 -0500

    Fixing login/signup bugs

--- server/server.js
+++ server/server.js
@@ -3,6 +3,18 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// Multer
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images')
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Data.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({ storage: storage });
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,6 +22,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.post('/upload', upload.single('imageURL'), (req, res) => {
+    res.send('Image Uploaded!');
+})
 
 
 if (process.env.NODE_ENV === 'production') {
